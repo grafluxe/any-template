80cf7e9dcfcd7be2339d462097f4f2e8860cedfd
Leandro Silva <lsilva@tremorvideo.com>
2016-12-11 17:58:57 -0500

    - Initial.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Any Template
+
+This Brackets extension allows you to easily create and use *your own* template files. It adds a new "Template" menu item to the main menu bar which lists any template files you create. Templates can be of any file type supported by Brackets.
+
+![Screenshot](./screenshot.gif)
+
+## Select Template
+
+- From the "Template" menu item, select your desired template file.
+  - Template files are sorted by type.
+- Once a file is selected, you will be presented with the following options:
+  - Create Untitled Document
+  - Overwrite Current Document
+  - Cancel
+
+## Add/Edit Templates
+
+- From the "Template" menu item, select "Templates Folder..."
+- Add any file type supported by Brackets.
+- Restart Brackets to see updated files.
+
+## Install Extension
+
+In the Brackets Extension Manager, either:
+
+- Search for Any Template
+- Use the "install from URL" option and paste in `https://github.com/Grafluxe/any-template`
+
+Restart Brackets after install.
+
+## License
+
+Copyright (c) 2016 Leandro Silva (http://grafluxe.com)
+
+Released under the MIT License.
+
+See LICENSE.md for entire terms.
---
+++ main.js
@@ -0,0 +1,153 @@
+/**
+ * @author Leandro Silva | Grafluxe, 2016
+ */
+
+/*global define, brackets */
+/*jshint unused:false */
+
+define((require, exports, module) => {
+  "use strict";
+
+  let AppInit = brackets.getModule("utils/AppInit"),
+      commandManager = brackets.getModule("command/CommandManager"),
+      menus = brackets.getModule("command/Menus"),
+      documentManager = brackets.getModule("document/DocumentManager"),
+      fileSystem = brackets.getModule("filesystem/FileSystem"),
+      fileUtils = brackets.getModule("file/FileUtils"),
+      dialogs = brackets.getModule("widgets/Dialogs"),
+      templateDir,
+      registeredFiles,
+      fileInfo,
+
+      getContents,
+      menuFiles,
+      menuOpenDir,
+      openModule,
+      readFile;
+
+  AppInit.appReady(() => {
+    menus.addMenu("Template", "anyTplMenu");
+    getContents();
+  });
+
+  getContents = () => {
+    templateDir = fileSystem.getDirectoryForPath(fileUtils.getDirectoryPath(decodeURI(module.uri)) + "templates/");
+
+    templateDir.getContents((a, files) => {
+      let match,
+          toSort = [];
+
+      files.forEach((el) => {
+        match = el.toString().match(/([\w\-. ']+)\.([\w\-. ']+)+\]$/);
+
+        if (match) {
+          toSort.push({
+            name: "<" + match[2] + "> " + match[1],
+            file: el,
+            type: match[2]
+          });
+        }
+      });
+
+      toSort.sort((a, b) => a.name.localeCompare(b.name));
+
+      menuFiles(toSort);
+      menuOpenDir();
+    });
+  };
+
+  menuFiles = (items) => {
+    registeredFiles = {};
+
+    items.forEach((el, i) => {
+      registeredFiles["anyTpl" + i] = el.file;
+
+      commandManager.register(el.name, "anyTpl" + i, () => {
+        fileInfo = {
+          id: "anyTpl" + i,
+          fType: el.type
+        };
+
+        openModule();
+      });
+
+      menus.getMenu("anyTplMenu").addMenuItem("anyTpl" + i);
+    });
+
+    //empty templates dir
+    if (items.length === 0) {
+      commandManager.register("- empty -", "anyTplNull", () => {
+        dialogs.showModalDialog(
+          "anyTplEmptyMsg",
+          "Set Template",
+          "Add files to your templates folder in order to make them available in the \"Template\" menu. <br>Once you add files, restart Brackets."
+        );
+      });
+
+      menus.getMenu("anyTplMenu").addMenuItem("anyTplNull");
+    }
+  };
+
+  menuOpenDir = () => {
+    commandManager.register("Templates Folder...", "anyTplDir", () => {
+      brackets.app.showOSFolder(templateDir.fullPath);
+    });
+
+    menus.getMenu("anyTplMenu").addMenuDivider();
+    menus.getMenu("anyTplMenu").addMenuItem("anyTplDir");
+  };
+
+  openModule = () => {
+    dialogs.showModalDialog(
+      "anyTplAction",
+      "Set Template",
+      "Which action would you like to take?",
+      [
+        {
+          id: "untitledFile",
+          text: "Create Untitled Document"
+        },
+        {
+          id: "overwriteFile",
+          text: "Overwrite Current Document"
+        },
+        {
+          id: "cancelFile",
+          text: "Cancel"
+        }
+      ]
+    ).done(readFile);
+  };
+
+  readFile = (id) => {
+    let count = documentManager.getAllOpenDocuments().length,
+        doc;
+
+    registeredFiles[fileInfo.id].read((err, data) => {
+      if (id === "cancelFile") {
+        return;
+      }
+
+      if (err) {
+        dialogs.showModalDialog(
+          "anyTplErrMsg",
+          "Set Template",
+          "There was an error loading your template file. Check your templates folder and restart Brackets."
+        );
+
+        console.error("Template Error:", err);
+        return;
+      }
+
+      if (id === "untitledFile") {
+        doc = documentManager.createUntitledDocument(count, "." + fileInfo.fType);
+
+        doc.setText(data);
+        documentManager.setCurrentDocument(doc);
+      } else if (id === "overwriteFile") {
+        documentManager.getCurrentDocument().setText(data);
+      }
+    });
+  };
+
+});
